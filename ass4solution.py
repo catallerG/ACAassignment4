36660e2633739343c978b8a68eb50ca0e1f16c26
mmcmullen7 <81434074+mmcmullen7@users.noreply.github.com>
2021-11-23 18:09:44 -0500

    Update ass4solution.py
    Added spectrogram function (lerch's) and started  the detect_key function

--- ass4solution.py
+++ ass4solution.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import scipy
+from scipy.fftpack import fft
 
 
 #A.Tuning Frequency Estimation
@@ -25,9 +26,26 @@ def block_audio(x, blockSize, hopSize, fs):
     return xb, t
 
 
+def compute_spectrogram(xb, fs):
+    numBlocks = xb.shape[0]
+    afWindow = 0.5 - (0.5 * np.cos(2 * np.pi / xb.shape[1] * np.arange(xb.shape[1])))
+    X = np.zeros([math.ceil(xb.shape[1]/2+1), numBlocks])
+    
+    for n in range(0, numBlocks):
+        # apply window
+        tmp = abs(fft(xb[n,:] * afWindow))*2/xb.shape[1]
+    
+        # compute magnitude spectrum
+        X[:,n] = tmp[range(math.ceil(tmp.size/2+1))] 
+        # normalize
+        X[[0,math.ceil(tmp.size/2)],n]= X[[0,math.ceil(tmp.size/2)],n]/np.sqrt(2) 
+
+    return X
+
+
 def estimate_tuning_freq(x, blockSize, hopSize, fs):
     xb, t = block_audio(x, blockSize, hopSize, fs)
-    X = scipy.fft(xb, blockSize=blockSize)
+    X = compute_spectrogram(xb, fs)
     spectralPeaks = get_spectral_peaks(X)
     tfInHz = []
     for i, value in enumerate(spectralPeaks):
@@ -65,4 +83,23 @@ def extract_pitch_chroma(X, fs, tfInHz):
     return pitchChroma
 
 
+def detect_key(x, blockSize, hopSize, fs, bTune):
+    
+    t_pc = np.array([[6.35, 2.23, 3.48, 2.33, 4.38, 4.09, 2.52, 5.19, 2.39, 3.66, 2.29, 2.88], [6.33, 2.68, 3.52, 5.38, 2.60, 3.53, 2.54, 4.75, 3.98, 2.69, 3.34, 3.17]])
+    
+    if bTune == 1:
+        tfInHz = estimate_tuning_freq(x, blockSize, hopSize, fs)
+    elif bTune == 0:
+        tfInHz = 440
+    else: 
+        raise NameError('Input Argument (bTune) should be either 0 or 1 ')
+    
+    xb = block_audio(x, blockSize, hopSize, fs)
+    X = compute_spectrogram(xb, fs)
+    pitchChroma = extract_pitch_chroma(X, fs, tfInHz)
+    
+    
+    for i in arrange(0, 1):
+        
+    return keyEstimate
 
